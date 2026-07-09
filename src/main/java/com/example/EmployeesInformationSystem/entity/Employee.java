bfc0be0252c9d4f25cb1ea83175d2ba6d8d86149
gireesh <potnurugireesh2357@gmail.com>
2026-07-09 14:50:34 +0530

    liberaues

--- src/main/java/com/example/EmployeesInformationSystem/entity/Employee.java
+++ src/main/java/com/example/EmployeesInformationSystem/entity/Employee.java
@@ -1,14 +1,6 @@
 package com.example.EmployeesInformationSystem.entity;
 
-import java.util.Date;
-import java.util.List;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+
 
 @Entity
 @Table(name="employee")
